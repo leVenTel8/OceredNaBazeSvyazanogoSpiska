922b7c3ee33f36ea9dd68db522f1cfd213d2b74f
Vitaly Frolov <leventel8@yandex.ru>
2017-02-24 13:56:12 +0400

    реализация очереди на двухстороннем связанном списке

---
+++ src/LinkQueue.java
@@ -0,0 +1,35 @@
+
+public class LinkQueue {
+
+    private FirstLastList theList;
+
+    public LinkQueue(){
+
+        theList = new FirstLastList();
+
+    }
+
+    public boolean isEmpty(){
+
+        return theList.isEmpty();
+
+    }
+    public void insert(long j){
+
+        theList.insertLast(j);
+
+    }
+    public long remove(){
+
+        return theList.deleteFirst();
+
+    }
+    public void displayQueue(){
+
+
+        System.out.print("Очередь (начало-->конец)");
+        theList.displayList();
+
+    }
+
+}
